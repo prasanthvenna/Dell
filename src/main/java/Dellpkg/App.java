5f5733a255e433602dced1105d8698cda83ce7ca
prasanthvenna <115454218+prasanthvenna@users.noreply.github.com>
2022-10-25 20:42:53 +0530

    Add files via upload

---
+++ src/main/java/Dellpkg/App.java
@@ -0,0 +1,13 @@
+package Dellpkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
